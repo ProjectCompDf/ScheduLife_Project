038e824a0f71fc8582f205a202f1bc82f58aad99
Daniel <danifain06@gmail.com>
2023-10-26 18:44:37 +0300

    test 2

--- app/src/main/java/com/example/schedulifeproject/MainActivity.java
+++ app/src/main/java/com/example/schedulifeproject/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -28,6 +29,6 @@ public class MainActivity extends AppCompatActivity
     public void SignUp(View view) {
          Users User=new Users(ED1.getText().toString(),ED2.getText().toString(),"gmail.com");
          refUsers.child(ED1.getText().toString()).setValue(User);
-
+        Toast.makeText(this, "You are now signed in", Toast.LENGTH_SHORT).show();
     }
 }
